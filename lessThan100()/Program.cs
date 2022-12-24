91fb3020e37f9cd8e2f0089de493e8a0bc76a465
j4nties <110736778+j4nties@users.noreply.github.com>
2022-12-24 04:45:21 +0300

    Add files via upload

---
+++ lessThan100()/Program.cs
@@ -0,0 +1,37 @@
+﻿// Creating Objects
+Problem test1 = new Problem();
+
+bool result;
+
+result = test1.lessThan100(1,4);
+result = test1.IsEqual(4,4);
+// Printing Variables
+Console.WriteLine(result);
+
+
+// Class
+class Problem
+{
+    // Method 
+    public bool IsEqual(int num1, int num2)
+    {
+        // if same numbers return true
+        if(num1==num2)
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
+    }
+    public bool lessThan100(int num1, int num2)
+    {
+        if( num1 + num2 >= 100)
+        {
+            return false;
+        }else
+        {
+            return true;
+        }
+    }
+}
