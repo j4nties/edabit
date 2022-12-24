1b925c2b8dbb76ac1c55c558df2e9d9a69f39a8e
j4nties <110736778+j4nties@users.noreply.github.com>
2022-12-24 16:26:05 +0300

    Add files via upload

---
+++ countTrue/Program.cs
@@ -0,0 +1,28 @@
+﻿//Creating Objects
+Problem test1 = new Problem();
+bool[] myArray = {true, false, false, true, false};
+int result = test1.countTrue(myArray);
+// Printing Variables
+Console.WriteLine(result);
+// Class
+class Problem
+{
+    //Method
+    public int countTrue(bool[] myArray)
+    {
+        //if array is empty {} return 0
+        int counter = 0;
+        if (myArray.Length == 0) 
+        {
+            return 0;
+        }
+        foreach (bool item in myArray)
+        {
+            //item == true
+            if(item){ 
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
