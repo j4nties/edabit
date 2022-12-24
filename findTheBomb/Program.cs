0592a944095e185326b12144a6a7208a6269eafd
j4nties <110736778+j4nties@users.noreply.github.com>
2022-12-24 16:57:49 +0300

    .Contains("bomb") if string include bomb word

---
+++ findTheBomb/Program.cs
@@ -0,0 +1,24 @@
+﻿//Creating Objects
+Problem object_1 = new Problem();
+
+string result = object_1.Bomb("There is a bomb.");
+
+// Printing Variables
+Console.WriteLine(result);
+
+
+// Class
+class Problem
+{
+    //Method checks if there is a bomb word in sentence
+    public string Bomb(string sentence) //detect the bomb
+    {
+        sentence = sentence.ToLower();  //converting string lowercase form
+        if (sentence.Contains("bomb")){
+            return "Duck!!!";
+        }else
+        {
+            return "There is no bomb, relax.";
+        }
+    }
+}
