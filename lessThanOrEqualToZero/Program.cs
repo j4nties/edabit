a075f5aa5b145386bae22272877668eabe44396e
j4nties <110736778+j4nties@users.noreply.github.com>
2022-12-24 17:03:27 +0300

    Add files via upload

---
+++ lessThanOrEqualToZero/Program.cs
@@ -0,0 +1,16 @@
+﻿//Creating Objects
+Problem object_1 = new Problem();
+
+var result = object_1.lessThanOrEqualToZero(5);
+
+// Printing Variables
+Console.WriteLine(result);
+
+// Class
+class Problem
+{   
+    public bool lessThanOrEqualToZero(double numb)
+    {
+        return numb<=0;
+    }
+}
