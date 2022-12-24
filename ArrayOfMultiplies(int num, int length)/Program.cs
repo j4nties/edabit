b601ba2787fb7df19646ec72e5a25acab08d2e7c
j4nties <110736778+j4nties@users.noreply.github.com>
2022-12-24 14:54:39 +0300

    Add files via upload

---
+++ ArrayOfMultiplies(int num, int length)/Program.cs
@@ -0,0 +1,22 @@
+﻿// Creating Objects
+Problem test1 = new Problem();
+
+var result = test1.ArrayOfMultiples(5,7);
+// Printing Variables
+Console.WriteLine(result);
+
+// Class
+class Problem
+{
+    // Method 
+    public string ArrayOfMultiples(int num, int length)
+    {
+        int[] myArray = new int[length];
+        for( int i = 0; i < length; i++ )
+        {
+            myArray[i] = (i+1) * num;
+            Console.WriteLine(myArray[i]);
+        }
+        return $"[{string.Join(", ", myArray)}]";
+    }
+}
